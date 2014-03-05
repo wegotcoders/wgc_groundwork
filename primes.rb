8c76044557fb00056eeaa953e5991c5f32497d4b
Anatoly A. Sanalaty <anatolisan@gmail.com>
2014-03-05 17:17:18 +0000

    finish basic prime_numbers function

--- primes.rb
+++ primes.rb
@@ -1,6 +1,16 @@
 class Primes
   def self.sum_to(limit = 100)
-    # TODO - add your prime number solution here...
-    "I'm working on it!"
+    total = 0;
+    (2..limit).each { |n| total+=n if is_p? n }
+    return total;
   end
+
+  # this method is to check if a given number can be devided 
+  # by any number that came before it.
+  # (technically, it just tells you if a given number is prime)
+  def self.is_p?(p)
+    (2...p).each { |n| return false if p % n == 0 };
+    return true;
+  end
+
 end
