b3fcdb34f40f404589f9e04ec10d81bc0f106279
MahmudH <mahmudhssn246@gmail.com>
2014-10-10 19:04:27 +0100

    website updated

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require './lib/profile'
 
-set :application_id, 'EDIT_ME'
-set :secret, 'EDIT_ME'
+set :application_id, '3f66cfdad4f14df45f90d74f7ca9d4d94a808e929136cdd0f4449c90fe4482df'
+set :secret, '88035a7be7eb3972f4ce0885b22b72b831cef36f7932bd65e4093138d76ae017'
 set :redirect_uri, 'http://localhost:4567/callback'
 set :site_url, 'http://wegotcoders.com'
 set :session_secret, 'secret'
@@ -16,6 +16,14 @@ get '/primes' do
   @sum = Primes.sum_to(limit)
 
   erb :primes, :layout => :main
+end
+
+get '/about' do
+  erb :about, :layout => :main
+end
+
+get '/ambition' do
+  erb :ambition, :layout => :main
 end
 
 get '/' do
