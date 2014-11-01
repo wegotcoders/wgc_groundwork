dc97b2979ff8f2821a6af96b076ffac023d5f82a
Richard Johnson <richard.j.c.johnson@gmail.com>
2014-11-01 13:14:34 +0000

    Hopefully all of my changes

--- primes.rb
+++ primes.rb
@@ -1,6 +1,9 @@
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
-    "I'm working on it!"
-  end
+    Prime.each(limit) do |i|
+    		 @sum += i
+    		 puts "@{sum}"
+    end
+end
 end
