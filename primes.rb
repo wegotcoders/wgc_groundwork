2b29467b3534c673fcb0fca05338a6ed4de86dc9
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-11-12 21:00:40 +0000

    added word count to all sections

--- primes.rb
+++ primes.rb
@@ -1,6 +1,9 @@
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
+    george = "george"
+      puts george
+      puts "poo"
     "I'm working on it!"
   end
 end
---
+++ public/javascript/prime.js
@@ -0,0 +1,31 @@
+$(function(){
+
+console.log("poo")
+
+
+  function getPrimeNumbers(){
+  // we need another copy of number 
+  // a prime number....
+    // two factors are 1 and itself... 
+      // we need to devide by something....
+      
+  var primeNumbers = []
+    num = 100
+    // find square route of num... 
+    var numSql = Math.sqrt(num);
+    console.log(numSql)
+    for(i=2; i<=numSql; i++){
+
+      if(num % i == 0){
+        // primeNumbers.push(i)
+        console.log(i)
+        // return false
+      }
+      // three and two? why do we need thes? ..... 
+    }
+    console.log(primeNumbers)
+  }
+   
+  getPrimeNumbers()
+
+})
