69d1b91a3bdf277ceb47bdcfdee33cc2d1b4084d
kknevitt <knev123@hotmail.com>
2014-11-21 15:17:31 +0000

    Added Some CSS and primes solution

--- primes.rb
+++ primes.rb
@@ -1,6 +1,31 @@
 class Primes
+
   def self.sum_to(limit = 100)
-    # TODO - add your prime number solution here...
-    "I'm working on it!"
+    primeTotal = 0
+
+  	for numberRange in 2...limit
+      if isPrime(numberRange) == true
+        primeTotal += numberRange
+      end
+  	end
+  	return primeTotal
   end
+
+  def self.isPrime(a)
+   prime = false
+   test = 0
+   for i in 2..Math.sqrt(a)
+    if a == 2
+      next
+    end
+        if a % i == 0
+          test += 1
+        end
+    end
+    if test == 0
+      prime = true
+    end
+    return prime
+  end
+
 end
