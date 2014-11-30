f736c790b91773c27379773af65fed2e5ace3478
Gouri <gouri.tr@gmail.com>
2014-11-30 17:04:52 +0000

    Primenumber solution plus tabbed Profile

--- primes.rb
+++ primes.rb
@@ -1,6 +1,36 @@
 class Primes
+
+def self.isprime(n)
+
+  is_prime = true
+  
+  for i in 2..n-1
+    if n % i == 0
+      is_prime = false
+  	end
+  end
+  
+ 	if is_prime == true
+    	return true
+  	else
+    	return false
+  	end
+  
+  end
+
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
-    "I'm working on it!"
+	sum = 0
+	num = 2
+    while num < limit
+    	
+    	if isprime(num)==true
+	    	sum+=num 
+	    end
+    num += 1
+    end
+    return sum
   end
+  
+ 
 end
