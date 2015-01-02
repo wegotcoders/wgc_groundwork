4173e5e1bb558f0cc9c90ff13a60b2e155a0423e
Jose Manuel <jmgcrespo@gmail.com>
2015-01-02 00:54:36 +0000

    Created sum_to method

--- primes.rb
+++ primes.rb
@@ -1,6 +1,30 @@
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
-    "I'm working on it!"
+    
+    sum=0
+    primes=[]
+    
+    case limit
+    when 1
+    	return 0
+    when 2 
+    	return 2
+    end
+
+    (3..limit).step(2).each do |n|
+    	
+    	if primes[n].nil?
+
+    		(3 * n..limit).step(n).each do |i|
+    			primes[i]=true
+    		end
+    	sum += n
+    	end
+
+    end
+    		    	
+    return sum + 2 
   end
+
 end
