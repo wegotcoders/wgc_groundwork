2188ebb682561365c1b63efae6daee33ed026c82
undersky0 <richardlonesteen@gmail.com>
2015-02-13 15:48:39 +0000

    some new methods in prime/tested, event listeners for main page

--- primes.rb
+++ primes.rb
@@ -8,4 +8,28 @@ class Primes
     return sum
   end
   
+  def self.multiples(n = 10)
+   i = 0
+   r = 0
+   while i < n do 
+      i+=1
+      if (i%3 == 0 or i%5 ==0)
+        r+=i
+      end
+    end
+      return r
+  end
+  def self.fibonacci(n)
+    a = [0]
+    n.times do |i|
+      if (i == 0)
+        a[i] = 0
+      elsif (i==1) 
+        a[i] = 1
+      else
+      a[i] = a[i-1] + a[i-2]
+      end
+    end
+   return a
+  end
 end
--- spec/primes_spec.rb
+++ spec/primes_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Primes do
 
   it "should caculate the sum of prime numbers to 100" do
-   
     expect(Primes.sum_to).to eq(1060)
   end
 
@@ -13,6 +12,38 @@ describe Primes do
 
   xit "should caculate the sum of prime numbers to 10000" do
     expect(Primes.sum_to(10_000)).to eq(5736396)
+  end
+  
+  it "should return Multiples of 3 and 5" do
+    n = Random.rand(1000)
+    i = 0
+    r = 0
+    while i < n do 
+      i+=1
+      if (i%3 == 0 or i%5 ==0)
+        r+=i
+      end
+    end
+    puts r
+    expect(Primes.multiples(10)).to eq(33)
+  end
+  
+  it "should return fibonacci numbers" do
+    n = 10
+    a = [0]
+    #Fn = F[n-1] + F[n-2]
+    n.times do |i|
+      if i == 0
+        a[i] = 0
+      elsif i==1 
+        a[i] = 1
+      else
+      a[i] = a[i-1] + a[i-2]
+      puts "fibonacci numbers: #{a[i]} "
+      end
+    end
+    expect(Primes.fibonacci(3)).to eq([0,1,1])
+    expect(Primes.fibonacci(4)).to match_array([0,1,1,2])
   end
 
 end
