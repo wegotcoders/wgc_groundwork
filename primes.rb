48f2e6a3691d6875ae46a29adbcd95516b538f88
resham14 <reshamgrg14@gmail.com>
2015-03-29 22:47:12 +0100

    Prime numbers less than 100

--- primes.rb
+++ primes.rb
@@ -1,6 +1,9 @@
-class Primes
-  def self.sum_to(limit = 100)
-    # TODO - add your prime number solution here...
-    "I'm working on it!"
-  end
+
+require 'prime'
+
+p = Prime.new
+(1..100).each do |n|
+  q = p.next()
+  puts "#{n}: #{q}"
 end
+
