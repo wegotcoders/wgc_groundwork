8483a3f937e2cfc6dacec968cc00eff72e63afed
resham14 <reshamgrg14@gmail.com>
2015-03-30 21:07:39 +0100

    sum of prime  numbers below 100

--- primes.rb
+++ primes.rb
@@ -1,9 +1,13 @@
 
-require 'prime'
 
-p = Prime.new
-(1..100).each do |n|
-  q = p.next()
-  puts "#{n}: #{q}"
+def prime?(n)
+  (2..n/2).none?{ |x| n % x == 0 }
 end
+
+primes = []
+for i in 2...100
+  primes.push(i) if prime?(i)
+end
+
+primes.inject(:+)
 
