d21c7504944707436b1a0f9ac04a08de5dc7c004
Adamantish <sonsequence@hotmail.com>
2015-07-01 12:29:15 +0100

    Figured out how to fetch a list of fields defined by array, wrote kids_age function to tell you how old I am. Did a tiny bit of styling.

--- app.rb
+++ app.rb
@@ -8,10 +8,19 @@ set :site_url, 'http://wegotcoders.com'
 set :session_secret, 'secret'
 enable :sessions
 
+def kids_age(dob, as_of = Time.now.utc.to_date ) 
+  adjuster = (as_of.month > dob.month || (as_of.month == dob.month && as_of.day >= dob.day)) ? 0 : 1
+  dob_years = as_of.year - dob.year - adjuster
+  days_since_bday = Date.new(1900, as_of.month, as_of.day) - Date.new(1900 - adjuster, dob.month, dob.day) 
+  quarter = (days_since_bday.to_f * 4 / 365).floor # Not perfect. Will be wrong if on a quarter boundary and if this is a leap year.
+  number_words = {1 => "a quarter", 2 => "a half", 3 => "three quarters"}
+  "Age #{dob_years}" << (quarter>0 ? ' and ' << number_words[quarter] : "")
+end
+
 get '/primes' do
   # TODO - Can we make this dynamic?
   @limit = params[:limit].to_i
-  @limit ||= 100
+
   # TODO - add your prime number solution in the primes.rb file.
   @sum = Primes.sum_to(@limit)
 
