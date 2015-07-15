23c2b65bdc038ce3e2937476cb05336b9a9037ae
denis89 <dedoin08@yahoo.de>
2015-07-15 11:46:23 +0200

    Update prime.rb
    Right. I've updated this file.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,11 @@
 require 'prime'
+array_of_primes = []
+(1..100).each do |n|
+    if Prime.prime?(n)
+        array_of_primes << n
+    end
+end
 
-puts Prime.first(100)
+answer = array_of_primes.inject(:+)
+
+puts "Sum of primes below #{100} is #{answer}."
