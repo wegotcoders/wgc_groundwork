84812321e63861892e5390c05c4f02373d065a68
skibinska <skibinska.ewelina@gmail.com>
2015-07-26 22:15:17 +0100

    menu & implementation of prime numbers sum calculator

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require './lib/profile'
 
+
 set :application_id, '689b5053c5c6a05d1bd9485aceff4f8707bce13b38e8849669047d4a499e6b72'
 set :secret, 'a54a53bb4b3fafbbda0a27c603a953d4da36f638cb211013889968043074fa49'
 set :redirect_uri, 'http://localhost:4567/callback'
@@ -8,12 +9,14 @@ set :site_url, 'http://wegotcoders.com'
 set :session_secret, 'secret'
 enable :sessions
 
-get '/primes' do
-  # TODO - Can we make this dynamic?
-  limit = 100
+get '/prime/input' do
 
-  # TODO - add your prime number solution in the primes.rb file.
-  @sum = Primes.sum_to(limit)
+  erb :prime_input, :layout => :main
+end
+
+get '/primes/:limit' do |limit|
+  
+  @sum = Primes.sum_to(limit.to_i)
 
   erb :primes, :layout => :main
 end
@@ -24,6 +27,14 @@ get '/' do
   end
 
   erb :index, :layout => :main
+end
+
+get '/edit' do
+  if signed_in?
+    @profile = trainee.get_profile
+  end
+
+  erb :edit, :layout => :main
 end
 
 post '/update' do
