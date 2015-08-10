5d5bbc5ab4dc908b722badb0b6601de398d14f93
Marvin M <marvin23m@gmail.com>
2015-08-10 12:36:18 +0100

    Completed Challenge to best of my ability

--- primes.rb
+++ primes.rb
@@ -1,6 +1,9 @@
+require 'prime'
+
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
-    "I'm working on it!"
+    Prime.each(100).reduce(:+)
+    
   end
 end
