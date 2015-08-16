327c8b260f687557cebff29594b2f23bbfba3da0
VoidAlpha321 <carmichaelbyron@yahoo.co.uk>
2015-08-16 10:27:23 +0100

    Update primes.rb

--- primes.rb
+++ primes.rb
@@ -1,6 +1,15 @@
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
-    "I'm working on it!"
+    require 'prime'
+
+p = Prime.new
+(1..100).each do |n|
+  q = p.next()
+  puts "#{n}: #{q}" 
+end
+
+Prime.each(100).reduce(:+)
+
   end
 end
