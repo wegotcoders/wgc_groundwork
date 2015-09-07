50e674366c48a081ff77fe3db00257367a45a5cc
Patricia Cupueran <pa1809ty@gmail.com>
2015-09-07 14:38:26 +0100

    sum of all prime numbers less than a given number, test using rspec command and primes sum field added

--- primes.rb
+++ primes.rb
@@ -1,6 +1,13 @@
+require 'prime'
+
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
-    "I'm working on it!"
+    #"I'm working on it!"
+    sum = 0
+    "limit must be an integer" unless limit.is_a? Integer 
+    "limit must be greater than 0" if limit <= 0
+    Prime.each(limit) {|prime| sum += prime}
+    sum
   end
 end
