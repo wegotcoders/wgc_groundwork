9b90dd9874c37f0c67b6a1d332fe55deae2f2433
Patricia Cupueran <pa1809ty@gmail.com>
2015-09-07 15:51:59 +0100

    Added an additional prime feature, get the largest prime factor of a given number

--- primes.rb
+++ primes.rb
@@ -10,4 +10,19 @@ class Primes
     Prime.each(limit) {|prime| sum += prime}
     sum
   end
+  def self.prime_factors(number = 100)
+  	array = Prime.prime_division(number)
+  	factors = []
+  	array.each { |sub_array| factors.push(sub_array[0]) }
+  	factors
+  end
+
+  def self.largest_prime_factor(number = 100)
+  	array = prime_factors(number)
+  	largest_factor = 1
+  	array.each do |num|
+  		largest_factor = num if num > largest_factor
+  	end
+  	largest_factor
+  end
 end
