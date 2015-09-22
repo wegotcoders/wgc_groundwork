66a26b72604e50272231c49850002d0a55e675ff
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2015-09-22 10:43:37 +0100

    Challenge upload

--- primes.rb
+++ primes.rb
@@ -1,6 +1,24 @@
 class Primes
   def self.sum_to(limit = 100)
-    # TODO - add your prime number solution here...
-    "I'm working on it!"
+    # TODO - add your prime number solution here...    
+    i = 2
+    sum = 0
+    until i > limit do 
+      j = has_prime?(i)
+    	sum += j
+      i += 1
+    end
+    return sum
   end
+
+  def self.has_prime?(num)
+		i = 2
+		until i > (num/2)
+			if num % i == 0
+				return 0
+			end
+	    i+=1
+		end
+		return num
+	end
 end
