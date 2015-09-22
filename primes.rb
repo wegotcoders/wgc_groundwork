4eaf9de35fcc7caa0837c69ffb855e6551f0dae1
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2015-09-22 10:47:48 +0100

    Edited for clarity

--- primes.rb
+++ primes.rb
@@ -1,23 +1,23 @@
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...    
-    i = 2
+    count = 2
     sum = 0
-    until i > limit do 
-      j = has_prime?(i)
-    	sum += j
-      i += 1
+    until count > limit do 
+      prime = has_prime?(count)
+    	sum += prime
+      count += 1
     end
     return sum
   end
 
   def self.has_prime?(num)
-		i = 2
-		until i > (num/2)
-			if num % i == 0
+		count = 2
+		until count > (num/2)
+			if num % count == 0
 				return 0
 			end
-	    i+=1
+	    count+=1
 		end
 		return num
 	end
