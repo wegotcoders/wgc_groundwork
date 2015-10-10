55fe72a438ac1c56af7d4206d07d9e03d21b4b0c
Kiran Woochit <kiranwoochit@hotmail.com>
2015-10-10 23:08:14 +0100

    Corrected updating profile

--- primes.rb
+++ primes.rb
@@ -1,6 +1,15 @@
 class Primes
   def self.sum_to(limit = 100)
-    # TODO - add your prime number solution here...
-    "I'm still working on it!"
-  end
+  	i = 0 
+  	prime_array = Prime.take(100)
+  	prime_array.each do |x|
+  		i = i + x
+	end
 end
+
+
+
+
+
+
+
