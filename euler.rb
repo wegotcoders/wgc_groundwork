2beada07f87a386be6aabea80b9ec3600723b595
Ketan Patel <ketpat@gmail.com>
2015-10-23 11:48:39 +0100

    Completed problem 2

--- euler.rb
+++ euler.rb
@@ -16,7 +16,29 @@ puts("Problem 1: Sum of multiples of 3 or 5 under 1000: #{multiples_of_3_or_5(10
 
 # Problem 2 - Even Fibonacci numbers
 
+def even_fibonacci_numbers
+  sum_of_even_numbers = 2
+  next_number = 0
+  sequence = [1,2]
+
+  while next_number < 4_000_000
+    next_number = sequence[sequence.count - 1] + sequence[sequence.count -2]
+    sequence.push(next_number)
+    puts (next_number)
+
+    if next_number % 2 == 0
+      sum_of_even_numbers += next_number
+    end
+  end
+
+  return sum_of_even_numbers
+end
+
+puts("Problem 1: Sum of Fibonacci even numbers less than 4 million: #{even_fibonacci_numbers}") #very slow
+
 # Problem 3 - Largest prime factor
+
+
 
 # Problem 4 - Largest palindrome product
 
