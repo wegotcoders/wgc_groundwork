555b0f867236b05a4b975539917e87e872e09da7
Ketan Patel <ketpat@gmail.com>
2015-10-23 09:14:21 +0100

    Implemented primes solution and added primes field to profile

--- primes.rb
+++ primes.rb
@@ -1,6 +1,25 @@
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
-    "I'm working on it!"
+    total = 0;
+
+    for i in 2..limit
+      if isPrime?(i)
+        total += i;
+      end
+    end
+
+    return total
+  end
+
+  def self.isPrime?(number)
+
+    for i in 2..number - 1
+      if(number % i == 0)
+        return false
+      end
+    end
+
+    return true
   end
 end
