e8a3528d54954f5f28a68279511b7caeaf78e4af
Sam Sargent <samsarge@hotmail.co.uk>
2015-11-03 22:59:10 +0000

    Finally fixed primes
    finally fixed primes, unable to refresh for the answer though it gets it
    upon the click onto the page.
    
    Some code is unable to be removed, not sure why - tried every other
    method I could think up and find however it just ended up making it
    unable to run app.rb

--- primes.rb
+++ primes.rb
@@ -1,6 +1,19 @@
+######CANT LAUNCH SERVER WITHOUT THIS CODE###########
 class Primes
-  def self.sum_to(limit = 100)
-    # TODO - add your prime number solution here...
-    "I'm working on it!"
-  end
+  def self.sum_to(limit)
+  # TODO - add your prime number solution here...
+    require 'prime'
+####################################################
+def self.sum_to(n)
+   (2..n/2).none?{ |x| n % x == 0 }
+end
+
+primes = []
+for i in 2...1000
+   primes.push(i) if self.sum_to(i)
+end
+#Prints the sum of the range - NOTE TO SELF - look into "inject" method more.
+primes.inject(:+)
+ end
+
 end
