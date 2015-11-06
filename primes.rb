0cec1f50de4d02e3d123a4b862b22e98f206b16b
Sam Sargent <samsarge@hotmail.co.uk>
2015-11-06 00:00:34 +0000

    Fixed Primes!
    A lot of confusion etc and going way back to basics I managed to crack
    the code.
    
    Also no more bugs when refreshing the page!!

--- primes.rb
+++ primes.rb
@@ -1,19 +1,19 @@
 ######CANT LAUNCH SERVER WITHOUT THIS CODE###########
 class Primes
-  def self.sum_to(limit)
-  # TODO - add your prime number solution here...
-    require 'prime'
-####################################################
-def self.sum_to(n)
-   (2..n/2).none?{ |x| n % x == 0 }
-end
+  def self.sum_to(number)
 
-primes = []
-for i in 2...1000
-   primes.push(i) if self.sum_to(i)
-end
-#Prints the sum of the range - NOTE TO SELF - look into "inject" method more.
-primes.inject(:+)
- end
+    start = 2
+    primes = (start..number).to_a
+    (start..number).each do |no|
+      (start..no).each do |num|
+        if ( no % num  == 0) && num != no
+          primes.delete(no)
+          break
+        end
+      end
+    end
+    primes.inject(:+)
+  end
+
 
 end
