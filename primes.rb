3f0018b62fc84ea295e1ff212bf4bfd4969702c6
Danny Santos <danny@ethicalswitch.com>
2016-02-27 10:55:54 +0000

    Added JS running word count. Added some responsive CSS

--- primes.rb
+++ primes.rb
@@ -1,8 +1,3 @@
-#get '/primes' do
-#    limit = params['prime_limit']
-#    puts params['prime_limit']    
-#end
-
 class Primes
   def self.sum_to(limit)
     # TODO - add your prime number solution here...
