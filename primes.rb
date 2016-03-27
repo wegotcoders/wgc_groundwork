ca065008ec43a912f54995201a7cdeb84ec0205d
kohrVid <kohrVid@zoho.com>
2016-03-27 17:21:00 +0100

    Refactored sum_to method but still have an issue with profile properties

--- primes.rb
+++ primes.rb
@@ -3,6 +3,6 @@ class Primes
   def self.sum_to(limit = 100)
 	  list_of_primes = []
 	  Prime.each(limit) { |prime| list_of_primes << prime }
-	  return list_of_primes.inject{|sum, x| sum + x}
+	  return list_of_primes.reduce(:+)
   end
 end
