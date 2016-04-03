99b41f96ae45b328a2b5cd8fd7ec686d4ee22f8b
Finn Francis <finn_francis@yahoo.com>
2016-04-03 16:29:13 +0100

    updated the prime number solution to be much more efficient

--- primes.rb
+++ primes.rb
@@ -1,15 +1,55 @@
 class Primes
-  def self.sum_to(limit = 100)
-    sum = 0
-		for i in (2..limit)
-		  prime = true
-		  for j in (2...i)
-		  	if i % j == 0
-		  		prime = false
-		  	end
-		  end
-		  sum += i if prime
+  
+	require 'prime'
+
+	def self.sum_to(limit = 100)
+		sum = 0
+		Prime.each(limit) do |prime|
+			sum += prime
 		end
-		return sum
-  end
+	end
+
+
+  #   This is the method I came up with to achieve the same thing
+  #   It's slower and for some reason doesn't work in this app,
+  #   even though it runs fine in it's own ruby file
+
+	# def prime_range(number)
+	# 	sqrt = Math.sqrt(number).to_i
+	# 	primes = [2]
+
+	# 	(3..sqrt).step(2) do |i|
+	# 		composite = false
+	# 		primes.each do |prime|
+	# 			if i % prime == 0
+	# 				composite = true
+	# 				break
+	# 			end
+	# 		end
+
+	# 		if primes[primes.length - 1] < i && !composite
+	# 				primes << i	
+	# 		end
+	# 	end
+	# 	return primes
+	# end
+
+ #  def self.sum_to(limit = 100)
+ #    return 0 if limit < 2
+	# 	sum = 0
+	# 	primes = prime_range(limit)
+	# 	primes.each do |prime|
+	# 		sum += prime
+	# 	end
+	# 	for i in(primes[primes.length - 1]..limit)
+	# 		skip = false
+	# 		primes.each do |prime|
+	# 			skip = true if i % prime == 0
+	# 			break if i % prime == 0         
+	# 		end
+	# 		sum += i unless skip
+	# 	end
+	# 	return sum
+ #  end
+
 end
