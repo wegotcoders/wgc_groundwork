bf01d30c3eb2f2f19e1ba7a962db9ac60bd91f19
Kieran <kieran.a.gibb@gmail.com>
2016-05-05 12:04:52 +0100

    Prime Number Solution added

--- primes.rb
+++ primes.rb
@@ -1,6 +1,25 @@
 class Primes
-  def self.sum_to(limit = 100)
-    # TODO - add your prime number solution here...
-    "I'm working on it!"
+  def self.sum_to(limit)
+    sum = 0
+    n = 1
+    while n < limit
+        n += 1
+        is_prime = true
+        if n == 1
+            is_prime = false
+        end
+
+        for i in 2..n-1
+            if n % i == 0
+                is_prime = false
+            end
+        end
+
+        if is_prime == true
+            sum += n
+        end
+    end
+
+    return sum
   end
 end
