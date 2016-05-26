6ac4e286ca2e84d1a0edf7e74e09eb0bc5a540e1
Nicholis Muller <nicholis.muller@gmail.com>
2016-05-26 17:21:15 +0200

    Initial commit

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require 'sinatra'
 require './lib/profile'
 
-set :application_id, 'EDIT_ME'
-set :secret, 'EDIT_ME'
+set :application_id, 'b5f294c431d513071d67200d65a4136602e7161d5b6688fbb88a2923a2bf5850'
+set :secret, '7f8d1053a50ab54dde9134ae5427aca0bdcd115e7c4d5c45fbec5b073f2963e7'
 set :redirect_uri, 'http://localhost:4567/callback'
 set :site_url, 'http://wegotcoders.com'
-set :session_secret, 'secret'
+set :session_secret, '7f8d1053a50ab54dde9134ae5427aca0bdcd115e7c4d5c45fbec5b073f2963e7'
 enable :sessions
 
 get '/primes' do
