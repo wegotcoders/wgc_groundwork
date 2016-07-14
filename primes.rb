957dad6cc5a9ab835f77a36821b65d0e7cddc1ca
TafDev <chombot@hotmail.com>
2016-07-14 11:33:35 +0100

    Adding my solution to primes

--- primes.rb
+++ primes.rb
@@ -1,6 +1,17 @@
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
+    primes = (2..limit).select do |n|
+    	(2..Math.sqrt(n)).none? do |i|
+    		(n%i).zero?
+    	end
+    end
 
-  end
+    	sum = primes.inject { |sum, i| sum + i }
+	end
 end
+
+Primes.sum_to(100)
+Primes.sum_to(1000)
+Primes.sum_to(10000)
+
