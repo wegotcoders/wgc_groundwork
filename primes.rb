7119249fb919a13d4a0a2f2669b87bf47becd31e
Georgina Heap <george.heap@gmail.com>
2016-09-29 19:08:08 +0100

    added .trimtrim to word count and attempting to do primes

--- primes.rb
+++ primes.rb
@@ -3,6 +3,35 @@ class Primes
   def self.sum_to(limit)
   	require 'prime'
   	Prime.each(limit).inject(:+)
+
+  #def self.sum_to(limit)
+  #primes = (0..limit).to_a
+
+  #primes[0] = primes[1] = nil
+
+  
+  #counter = 0
+  	#primes.each do |n|
+   
+    	#next unless n
+
+    	#break if n*n > limit
+    	#counter += 1
+    
+    	#(n*n).step(limit,p) { |m| primes[m] = nil }
+  	#end
+
+  	#primes.compact
+
+  	#@sum=0
+  	#primes.each{ |n| @sum+= n}
+  #end
+
+#My attempt at getting a more worked out prime but it is not working yet
+
+
+
+
   end
 end
 
