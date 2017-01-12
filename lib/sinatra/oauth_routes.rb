70c6dd014984d48b2af9c4be33f1a4137a4451c2
Bartlomiej Laskowski <frameture@gmail.com>
2017-01-12 14:57:22 +0100

    Workaround. Fetching data from server is usuccessful. I fetch it from hash.

--- lib/sinatra/oauth_routes.rb
+++ lib/sinatra/oauth_routes.rb
@@ -14,18 +14,22 @@ module Sinatra
       end
 
       def access_token
+          puts "Sinatra.access_token, self: #{self}"
         OAuth2::AccessToken.new(client, session[:access_token], :refresh_token => session[:refresh_token])
       end
 
       def signed_in?
+          puts "\n Oauth.signed_in?\n"
         !session[:access_token].nil?
       end
 
       app.get '/authorise' do
+        puts "\n Sinatra.app.get'/authorise': redirect_uri: #{settings.redirect_uri} \n"
         redirect client.auth_code.authorize_url(:redirect_uri => settings.redirect_uri)
       end
 
       app.get '/callback' do
+          puts "\n Sinatra.app.get'/callback' #{self}\n"
         if params["error"]
           @errors = Hash.new[params["error"]]= params["error_description"]
           erb :error, :layout => :main
--- lib/we_got_coders/trainee.rb
+++ lib/we_got_coders/trainee.rb
@@ -7,16 +7,31 @@ module WeGotCoders
     def initialize(url, access_token)
       self.class.base_uri url
       self.access_token = access_token
+      puts "\n initializing Trainee: {self} \n"
     end
-
+    
+    ### modified
+    def get_profile
+        @get_profile ||= { 
+          first_name: "Bartlomiej",
+          last_name: "Laskowski",
+          age: 22,
+          status: "Student" }
+    end
+    #############
+    
+### original get_profile method
+=begin 
     def get_profile
       response = self.class.get("/trainees/profile?access_token=#{access_token}")
-      if response.code == 200
-        @get_profile ||= JSON.parse(response.body)
-      else
-        raise Exception.new("Could not find profile. Is the site_url correct?")
-      end
-    end
+       puts "\n code is: #{response.code}, response is: #{response} \n"
+       if response.code == 200
+         @get_profile ||= JSON.parse(response.body)
+       else
+         raise Exception.new("Could not find profile. Is the site_url correct?")
+       end
+     end
+=end
 
     def update_profile(params)
       response = self.class.put("/trainees/profile?access_token=#{access_token}", :body => params)
