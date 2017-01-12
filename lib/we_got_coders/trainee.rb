3cb5dcb2a6bd92230efd51b6eacd83af8772c874
Bartlomiej Laskowski <frameture@gmail.com>
2017-01-12 20:09:00 +0100

    Adding more information to the hash.

--- lib/we_got_coders/trainee.rb
+++ lib/we_got_coders/trainee.rb
@@ -13,10 +13,57 @@ module WeGotCoders
     ### modified
     def get_profile
         @get_profile ||= { 
-          first_name: "Bartlomiej",
-          last_name: "Laskowski",
+          name: "Bartlomiej Laskowski",
           age: 22,
-          status: "Student" }
+          webpage: "<a href='https://frameture.github.io'>frameture.github.io</a> ",
+          email: "frameture@gmail.com",
+          phone: "0048 794 668 294",
+          status: "Student" ,
+          about: 
+              "Despite the fact that I am 22, I have begun my studies last year. I study 
+              computing at Arden University (distance learning). After I have finished 
+              my high school, I was looking for the best path for myself so, I have 
+              travelled and worked.  I have spent whole 2015 in Copenhagen working 
+              in different places mostly in hospitality industry. In the late 2015 I have 
+              started my journey with programming and got in love with it. I have spent 
+              almost all 2016 on learning programming. In 2016 I have also worked as 
+              an assistant to the local agriculture import-export company. My main 
+              responsibility was to translate during dialogs and negotiations with 
+              overseas business partners. The job also included number of abroad 
+              business trips.",
+          
+          programming_project: 
+            "I have not taken part in any collaborative project as I was learning 
+            everything myself. But I have developed one, my personal webpage 
+            hosted on GitHub: <a href='https://frameture.github.io' target='_blank'>frameture.github.io</a> 
+            I have developed it with using HTML, CSS and JavaScript with jQuery. It 
+            was not difficult as it only contains few features, which rely mostly on 
+            user-events. I tried to use all best-practices and conventions I know, 
+            but I have begun web development not long ago and do not have yet
+            much knowledge in this field. One of the more demanding things in 
+            this project was to use SVG for drawing ‘programming-log’ feature as it 
+            was first time I have used it.",
+          
+          why: 
+            "I am seeking a career in web development and decided that I will take 
+            part in coding bootcamp. I know fundamentals of programming and 
+            already know Java quite well so, I know that I will take a lot of knowledge 
+            from such camp. After research I have decided that ‘We Got Coders’ is 
+            the best fit as you teach Ruby with Rails, which I would like to learn, 
+            and also offer real experience with working on client-based projects.",
+          
+          one_year_time: 
+            "In 1 year’s time, I see myself working as a full-stack developer and 
+            getting skills, experience and knowledge to be promoted a senior position.",
+            
+          learning_experience: 
+            "I have been learning programming for a year and I value best resources 
+            and practicing most. But I have missed good advices from experienced
+            programmers, which would help greatly. I would also say that working
+            in collaboration with others would make my learning experience better.
+            For a commercial training it also fits great to offer work placements as 
+            you do, and I think that would be the best part of it." 
+        }
     end
     #############
     
