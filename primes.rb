da5d92d1c57a4bf6516d7de7b4e2fff5347900ba
cristina <cristina.radu3@gmail.com>
2017-03-23 21:58:46 +0000

    First commit

--- primes.rb
+++ primes.rb
@@ -1,6 +1,14 @@
 class Primes
   def self.sum_to(limit = 100)
-    # TODO - add your prime number solution here...
+    primes = (2..100).select do |n| 
+    (2..Math.sqrt(n)).none? do |i|
+       (n % i).zero?
+    end
+
+  sum = primes.reduce(:+)  
+  
+  puts total_count
+
     "I'm working on it!"
   end
 end
