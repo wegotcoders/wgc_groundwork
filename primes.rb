c213d6a18d1f23ac415ea3725e2a80f8488f85ba
mjjackman <mjjackman2000@yahoo.co.uk>
2017-04-14 16:53:24 +0100

    Added code to primes.rb to find the sum of the prime numbers.

--- primes.rb
+++ primes.rb
@@ -1,8 +1,16 @@
+require 'prime'
 class Primes
   def self.sum_to(limit = 100)
+    total = 0
+    primeSum = []
     # TODO - add your prime number solution here...
-
-
+    (2..limit).each do |i|
+      if Prime.prime?(i)
+        primeSum.push(i)
+        total += i
+      end
+    end
+      return total
     "I'm working on it man!"
-  end
+end
 end
