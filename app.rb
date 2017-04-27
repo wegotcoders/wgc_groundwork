7a4966a154b0ba083d2e4f64a67a2c460e51f1f4
Sophie Figgis <sophiefiggis@gmail.com>
2017-04-27 22:38:26 +0100

    first commit

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require './lib/profile'
 
-set :application_id, 'EDIT_ME'
-set :secret, 'EDIT_ME'
+set :application_id, '22604679c625c388f68afe3044c21a7d85303998a740d61ab4eb3d1981ca6884'
+set :secret, '78ddb0e3d056d58ce7399b9577e5e99f26006639f87dea562e5a2d472920da8b'
 set :redirect_uri, 'http://localhost:4567/callback'
 set :site_url, 'https://wegotcoders.com'
 set :session_secret, 'secret'
@@ -10,9 +10,12 @@ enable :sessions
 
 get '/primes' do
   # TODO - Can we make this dynamic?
-  limit = 100
+  #limit = 100
+  limit = params[:limit].to_i
+  @number = limit
 
   # TODO - add your prime number solution in the primes.rb file.
+
   @sum = Primes.sum_to(limit)
 
   erb :primes, :layout => :main
--- primes.rb
+++ primes.rb
@@ -1,6 +1,13 @@
+require 'prime'
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
-    "I'm working on it!"
-  end
+    "Different words to see what is going on."
+    #
+    n = 0
+    Prime.each(limit) do |prime|
+    	n += prime
+    end
+    p n
+end
 end
