929f34465643dad82581e83c29d87606124db062
TafDev <chombot@hotmail.com>
2017-05-05 11:48:06 +0100

    Just testing

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require './lib/profile'
 
-set :application_id, 'EDIT_ME'
-set :secret, 'EDIT_ME'
+set :application_id, 'f68389f726e938826f5b664b696de18cc4cc952683052a267bab7d30b13a46d2'
+set :secret, 'e0da34a4ac9a1a172fcbb6a6e756bf03217aa5e38494b387764fd63aa0fc8c54'
 set :redirect_uri, 'http://localhost:4567/callback'
 set :site_url, 'https://wegotcoders.com'
 set :session_secret, 'secret'
