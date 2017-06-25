b048c8027f90cbe97424165ceda42d2cb1acab07
CorinaFeraru <corina_feer@yahoo.com>
2017-06-26 00:02:50 +0100

    updated primes.rb

--- primes.rb
+++ primes.rb
@@ -3,8 +3,8 @@ class Primes
     # TODO - add your prime number solution here...
 	  require 'prime'
 prime_array = Prime.take_while{|p| p < 100}
-total_count = prime_array.inject{|sum,items| sum + items}
-p total_count
+sum_count = prime_array.inject{|sum,items| sum + items}
+p sum_count
 	  
     "1060"
   end
