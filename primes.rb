f76ab5e36adbee8f4cfb98f1bad6ad47948a46bd
CorinaFeraru <corina_feer@yahoo.com>
2017-06-25 23:45:31 +0100

    Updated the primes sum from 100

--- primes.rb
+++ primes.rb
@@ -1,6 +1,11 @@
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
-    "I'm working on it!"
+	  require 'prime'
+prime_array = Prime.take_while{|p| p < 100}
+total_count = prime_array.inject{|sum,items| sum + items}
+p total_count
+	  
+    "1060"
   end
 end
