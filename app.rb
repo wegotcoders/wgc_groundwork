6773bbe1201583e4208873584a4a6863efe2ef0f
CorinaFeraru <corina_feer@yahoo.com>
2017-06-25 23:56:14 +0100

    update app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 require 'sinatra'
 require './lib/profile'
 
-set :application_id, 'EDIT_ME'
-set :secret, 'EDIT_ME'
+set :application_id, 'a540c221a04199d5b62a53d9914ae968a5602d5c31aa6f8e42e72f377c3591a5'
+set :secret, '3671f33b988d603cf1054032dad7cd577c0d97cdfb557944cfd9cba8055a0798'
 set :redirect_uri, 'http://localhost:4567/callback'
 set :site_url, 'https://wegotcoders.com'
 set :session_secret, 'secret'
