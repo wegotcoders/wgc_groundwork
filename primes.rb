7fa59568330a2de35a6b012ef2f9613f8e716894
CorinaFeraru <corina_feer@yahoo.com>
2017-06-26 21:12:10 +0100

    444

--- primes.rb
+++ primes.rb
@@ -1,5 +1,5 @@
-class Primes
-  def self.sum_to(limit = 100)
+#class Primes
+  #def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
 	  require 'prime'
 prime_array = Prime.take_while{|p| p < 100}
@@ -11,5 +11,4 @@ millions_count = second_array.inject {|amount,digits| amount + digits }
 p millions_count
 	  
     "The results are 1,060 and 142,913,828,922"
-  end
-end
+ 
