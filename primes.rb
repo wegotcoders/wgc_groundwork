50cdf31ba06a6f8b8212b5d8b0b2e7ba1adfbbf0
CorinaFeraru <corina_feer@yahoo.com>
2017-06-26 21:03:11 +0100

    22222

--- primes.rb
+++ primes.rb
@@ -1,17 +1,6 @@
-   
-
-
-# TODO - add your prime number solution here...
-
-	  require 'prime'
-first_array = Prime.take_while {|p| p < 100 }
-sum_count = first_array.inject {|sum,items| sum + items }
-p sum_count
-	  
-
-second_array = Prime.take_while {|q| q < 2000000 }
-amount_count = second_array.inject {|amount,digits| amount + digits }
-p amount_count
-
-	   "1,060 and 142,913,828,922!"
- 
+class Primes
+  def self.sum_to(limit = 100)
+    # TODO - add your prime number solution here...
+    "I'm working on it!"
+  end
+end
