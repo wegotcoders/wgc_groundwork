54b6176154f849a97aac45929a6e58a98d802b58
CorinaFeraru <corina_feer@yahoo.com>
2017-06-26 20:54:58 +0100

    updated

--- primes.rb
+++ primes.rb
@@ -1,9 +1,17 @@
-class Primes
-  def self.sum_to(limit = 100)
-    # TODO - add your prime number solution here...
+   
+
+
+# TODO - add your prime number solution here...
+
 	  require 'prime'
-first_array = Prime.take_while{|p| p < 100}
-sum_count = prime_array.inject{|sum,items| sum + items}
+first_array = Prime.take_while {|p| p < 100 }
+sum_count = first_array.inject {|sum,items| sum + items }
 p sum_count
-  end
-end
+	  
+
+second_array = Prime.take_while {|q| q < 2000000 }
+amount_count = second_array.inject {|amount,digits| amount + digits }
+p amount_count
+
+	   "1,060 and 142,913,828,922!"
+ 
