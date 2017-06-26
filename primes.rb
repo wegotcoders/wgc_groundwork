0c90a725c1c006c9595c4819d1ab224045d3ba27
CorinaFeraru <corina_feer@yahoo.com>
2017-06-26 19:51:08 +0100

    0

--- primes.rb
+++ primes.rb
@@ -2,10 +2,8 @@ class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
 	  require 'prime'
-prime_array = Prime.take_while{|p| p < 100}
+first_array = Prime.take_while{|p| p < 100}
 sum_count = prime_array.inject{|sum,items| sum + items}
 p sum_count
-	  
-    "1060"
   end
 end
