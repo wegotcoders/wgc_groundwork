5532a0556083ca8dc43b4ee7199abcbd3d26d636
Joeffrey Lamy <joeffreylamy41@gmail.com>
2017-07-18 23:27:14 +0100

    Add Prime Sum to edit profile section, edit prime sum to 2M value, add code draft for sum of prime to a hundred

--- primes.rb
+++ primes.rb
@@ -1,6 +1,15 @@
+=begin
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
     "I'm working on it!"
   end
 end
+=end
+
+primes_to_hundred = Prime.take_while {|p| p < 100}
+
+total = 0
+
+primes_to_hundred.each { |x| total +=x }
+puts total
