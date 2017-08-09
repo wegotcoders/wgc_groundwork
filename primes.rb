6d803461ada17481029871967c56e69daf6659cd
Ethaning <ethanjcarter@hotmail.co.uk>
2017-08-09 16:58:02 +0100

    second commit

--- primes.rb
+++ primes.rb
@@ -1,6 +1,16 @@
+require "prime"
 class Primes
   def self.sum_to(limit = 100)
     # TODO - add your prime number solution here...
-    "I'm working on it!"
+    sum = 0
+    i = 1
+	loop do
+		break if i > limit
+		sum += i if Prime.prime?(i)
+	end
+	puts sum
+	sum
   end
+
+  puts Primes()
 end
